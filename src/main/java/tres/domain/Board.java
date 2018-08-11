fde67db316e924253dc44cf3d6015e3d2b2bfcf6
rugwiza <erickrugwiza@gmail.com>
2018-08-11 20:13:42 +0200

    Board Table added, Please The one who worked on User Management add the foreign key from Board as UserToBoardAssignment

---
+++ src/main/java/tres/domain/Board.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tres.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Gwiza
+ */
+@Entity
+@Table(name = "Board")
+@NamedQuery(name = "Users.findAll", query = "SELECT r FROM Board r order by boardId desc")
+public class Board extends CommonDomain implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	@Column(name = "boardId")
+	private int boardId;
+
+	/* this is for boardName */
+	@Column(name = "boardName", unique = true)
+	private String boardName;
+
+	/* this is for description */
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "creationDate", columnDefinition = "DATETIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date creationDate;
+
+	@ManyToOne
+	@JoinColumn(name = "institution")
+	private Institution institution;
+
+	@ManyToOne
+	@JoinColumn(name = "board")
+	private Board board;
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getBoardName() {
+		return boardName;
+	}
+
+	public void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Institution getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(Institution institution) {
+		this.institution = institution;
+	}
+
+
+}
