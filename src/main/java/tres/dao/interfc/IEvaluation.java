939f704ba149624c68927137f550dd18618cab4c
RICHARD MWENEDATA <richardmwenedata01@gmail.com>
2018-10-03 12:31:45 +0200

    I committed the following about reports

---
+++ src/main/java/tres/dao/interfc/IEvaluation.java
@@ -0,0 +1,17 @@
+package tres.dao.interfc;
+
+import java.util.List;
+
+
+import tres.domain.Evaluation;
+
+public interface IEvaluation {
+	public Evaluation saveEvaluation(Evaluation evaluation);
+
+	public List<Evaluation> getListEvaluations();
+
+	public Evaluation getEvaluationById(int EvaluationId, String primaryKeyclomunName);
+
+	public Evaluation UpdateEvaluation(Evaluation evaluation);
+
+}
