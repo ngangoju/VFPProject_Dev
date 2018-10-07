046e35b9dced4a0865ef1139c68985d4829247b7
rugwiza <erickrugwiza@gmail.com>
2018-10-07 09:01:30 +0200

    organigram of board ::Gwiza

---
+++ src/main/java/tres/vfp/dto/BoardDto.java
@@ -0,0 +1,91 @@
+package tres.vfp.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import tres.domain.Board;
+import tres.domain.Institution;
+import tres.domain.Users;
+
+public class BoardDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int boardId;
+	private String boardName;
+	private String description;
+	private Date creationDate;
+	private Institution institution;
+	private Board board;		
+
+	private boolean editable;
+	
+	public boolean isEditable() {
+		return editable;
+	}
+
+	public void setEditable(boolean editable) {
+		this.editable = editable;
+	}
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getBoardName() {
+		return boardName;
+	}
+
+	public void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Institution getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(Institution institution) {
+		this.institution = institution;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	
+	
+	
+	
+}
+
+
