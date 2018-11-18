e4d7604137a10e5e05af1a97ab5b492ae0ca9bbf
rugwiza <erickrugwiza@gmail.com>
2018-11-18 13:50:26 +0200

    Board commit ::Gwiza

--- src/main/java/com/vfp/tres/Organigram.java
+++ src/main/java/com/vfp/tres/Organigram.java
@@ -41,32 +41,25 @@ public class Organigram {
 			} else {
 				t.setParent(x.getBoard().getBoardId());
 			}
-
 			list2.add(t);
 		}
 		this.jsonData = new Gson().toJson(list2);
 	}
-
 	public List<Board> getBoardList() {
 		return boardList;
 	}
-
 	public void setBoardList(List<Board> boardList) {
 		this.boardList = boardList;
 	}
-
 	public String getJsonData() {
 		return jsonData;
 	}
-
 	public void setJsonData(String jsonData) {
 		this.jsonData = jsonData;
 	}
-
 	public int getChildId() {
 		return childId;
 	}
-
 	public void setChildId(int childId) {
 		this.childId = childId;
 	}
